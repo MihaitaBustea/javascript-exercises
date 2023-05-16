cc5aaa742055ff4985b81d99ecab0dab330ebf93
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-16 20:43:02 +0300

    Solve the reverseString exercise to run all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+function reverseString(str) {
+    let finalString = "";
+    let charList = str.split("");
+    charList.reverse();
+    for (const char of charList) {
+        finalString += char;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
