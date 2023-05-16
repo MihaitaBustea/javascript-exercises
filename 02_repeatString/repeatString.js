656ec265c384d0c18c7fdd440111ffe0c53cc233
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-16 20:29:19 +0300

    Solve the repeatString exercise to run all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+function repeatString(str, num) {
+    let stringToAdd = ""
+    if (num < 0) {
+        return "ERROR";
+    } else if (num === 0) {
+        return "";
+    } else {
+        while (num > 0) {
+            stringToAdd += str;
+            num--;
+        }
+        return stringToAdd;
+    }
+    
 };
 
 // Do not edit below this line
