64179d3b2322083f1a4e1d2456d46c22de340347
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-17 20:43:15 +0300

    Solve the leapYears exercise to run all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+function leapYears(num) {
+    let leapYearStatus = undefined;
+    if (typeof num !== "number"){
+        return "ERROR";
+    } else {
+        if (num%400 === 0 || num%4 === 0 && num%100 !== 0){
+            leapYearStatus = true;
+        } else {
+            leapYearStatus = false;
+        }
+    }
+    return leapYearStatus;
 };
 
 // Do not edit below this line
