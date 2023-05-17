8806e9eb0e64e61dc1b43e00854e611f19fcbffb
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-17 20:42:31 +0300

    Solve the removeFromArray exercise to run all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+function removeFromArray(arr, ...args) {
+    let index = 0;
+    for (const number of args){
+        for (const element of arr) {
+            if (number === element ) {
+                index = arr.indexOf(element);
+                arr.splice(index, 1);
+            }
+        }    
+    }
+    return arr;
 };
 
 // Do not edit below this line
