2197d3c2d41cb3b802e3c0a070aa31eb2e80ea5c
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-17 20:42:55 +0300

    Solve the sumAll exercise to run all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+function sumAll(x, y) {
+    let sum = 0;
+    if (typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    } else if (x < 0 || y < 0){
+        return "ERROR";
+    } else {
+        let smallNum = 0;
+        let bigNum = 0;
+        if (x >= y){
+            smallNum = y;
+            bigNum = x;
+        } else {
+            smallNum = x;
+            bigNum = y;
+        }
+        for (let i = smallNum; i <= bigNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
